49fab7701c28f3c2fc1f9ff1818c280d5fe5ffca
Abhay Pande <abhaypande@abhays-mbp.lan>
2020-04-18 18:28:26 -0700

    added command definitions for while language

--- while-inter/ast/cmd.py
+++ while-inter/ast/cmd.py
@@ -1,15 +1,17 @@
 class Cmd:
-    def execute(self,env):
+    def execute(self, env):
         raise NotImplementedError()
 
 
 class Skip(Cmd):
     def __init__(self):
+        pass
 
     def execute(self,env):
         return env
 
-class AssignExpr(Cmd):
+
+class Assign(Cmd):
     def __init__(self, variable, expr):
         self.variable = variable
         self.expr = expr
@@ -17,4 +19,45 @@ class AssignExpr(Cmd):
     def execute(self,env):
         env[self.varialbe.eval()] = self.expr.eval()
         return env
+
+
+class Compound(Cmd):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def execute(self,env):
+        env = self.first.execute(env)
+        env = self.second.execute(env)
+        return env
+
+
+class If(Cmd):
+    def __init__(self, condition, true_exp, false_exp):
+        self.condition = condition
+        self.true_exp = true_exp
+        self.false_exp = false_exp
+
+    def execute(self,env):
+        if self.condition:
+            return self.true_exp.execute(env)
+        else:
+            return self.false_exp.execute(env)
+
+
+class While(Cmd):
+    def __init__(self, condition, command):
+        self.condition = condition
+        self.command = command
+
+    def execute(self,env):
+        if self.condition:
+            env = self.command.execute(env)
+            return While(self.condition,self.command).execute(env)
+        else:
+            return env
+
+
+
+
 
